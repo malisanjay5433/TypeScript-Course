89a0e15e621425c57d245803b7b7ca830686c3ab
Sanjay Mali <malisanjay5433@gmail.com>
2022-11-22 14:57:43 +0530

    Chore : all about tuples

---
+++ mytuples.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+//Tuples 
+var tokenExpiry;
+tokenExpiry = [401, "token expiry"];
+var newUser = ["sanjay", "san@gmail.com", false];
+newUser[0] = "Mali";
+newUser[1] = "mali@gmail.com";
+newUser[2] = true;
+newUser.pop();
+newUser.push();
