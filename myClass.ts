8a05b126d044c66bb83f21b760f4401cea01b65c
Sanjay Mali <malisanjay5433@gmail.com>
2022-11-22 20:11:12 +0530

    Chore : private methods

--- myClass.ts
+++ myClass.ts
@@ -13,17 +13,32 @@ export{}
 
 
 //Other way to write classes, mean in professional world ppl who write such way.
+//By using public in constructor don't need to use classic way of declartion, Pl check first example.
 class User{
     readonly place = "Mumbai"
+    private courseCount = 1
     constructor(
         public name :string, 
         public email:string,
         readonly city:string
         ){
-    }
 }
-
+private deleteToken() {
+    console.log("Token is deleted!!!");
+    
+}
+get getCourseCount() : number{
+    return this.courseCount;
+}
+set setCourseCount(courseNum){
+    if (courseNum <= 1){
+        throw new Error("Course count shoudd more than 1")
+    }
+    this.courseCount = courseNum
+ }
+}
 
 
 const obj = new User("Sanjay Mali", "sanjay@gmail.com","Mumbai")
 console.log(`User object  ${{obj}}`);
+//Getter and Setters
